e50be0529c912b8e049b047e0777d19f33f236e0
Khang Tran <khang.tran@hcl.com>
2022-10-01 12:28:19 +0700

    Setup Jenkins

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { router as movieRouter } from './movie/index.js';
-
+import morgan from 'morgan';
+import { createWriteStream } from 'fs';
 const app = express();
 
+const accessLogStream = createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('comon', { immediate: true, stream: accessLogStream}));
 app.use('/movie', movieRouter);
 
 app.get('/', (request, response)  => {
